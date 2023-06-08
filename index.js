ed8f603f3ce15ca4e5a76330a9412910ed6f1c08
JUNIOR RODRIGUES <juniorrodrigues1717@gmail.com>
2023-06-07 23:36:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.post('/send-sms', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 7032;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
