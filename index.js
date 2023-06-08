43a19373bb532dd9fcc2d7f3b7a0fab2e0d0151b
JuniorRodrigu <juniorrodrigues1717@gmail.com>
2023-06-08 19:33:43 -0300

    mais umtes

--- index.js
+++ index.js
@@ -35,4 +35,9 @@ app.post('/send-sms', async (req, res) => {
   }
 });
 
+const PORT = process.env.PORT || 9001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 module.exports = app;
