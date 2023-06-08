a90d88b7454b5dd5ab87db993e57b0776407a6a3
JuniorRodrigu <juniorrodrigues1717@gmail.com>
2023-06-08 19:58:28 -0300

    TEST

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.post('/send-sms', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 9001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
