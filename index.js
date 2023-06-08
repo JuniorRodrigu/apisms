342e64e708548aed7fcdf8e7bc29d79b7aeb8e69
JuniorRodrigu <juniorrodrigues1717@gmail.com>
2023-06-08 18:06:28 -0300

    deu certo

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 9001;
-
-// Rota para exibir "Olá mundo"
-app.get('/', (req, res) => {
-  res.send('Olá mundo!');
-});
 
 const bodyParser = require('body-parser');
 const twilio = require('twilio');
+const express = require('express');
 const cors = require('cors');
 
+const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-
 const accountSid = 'ACca6ad5ddc85bc3e89dbf60d1cdac693a';
-const authToken = 'ce0dad0c282c8e875701e261a5b0f202';
+const authToken = '77c9903ee0585f75f2397a3a7e7745b6';
 const client = new twilio(accountSid, authToken);
 
 app.post('/send-sms', async (req, res) => {
   const { numero } = req.body;
+
+  console.log('Request body:', req.body);
 
   try {
     const message = await client.messages.create({
@@ -28,12 +23,16 @@ app.post('/send-sms', async (req, res) => {
       to: numero,
     });
 
+    console.log('Twilio response:', message);
     res.status(200).send(message.sid);
   } catch (error) {
+    console.error('Error:', error);
     res.status(500).send(error.message);
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
