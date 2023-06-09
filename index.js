6d0e73ad0c046a2e2dcf16c5caa41497437136a3
JUNIOR RODRIGUES <juniorrodrigues1717@gmail.com>
2023-06-09 07:09:05 -0300

    Atualizar o index.js

--- index.js
+++ index.js
@@ -1,45 +1,46 @@
-const bodyParser = require('body-parser');
-const twilio = require('twilio');
-const express = require('express');
-const cors = require('cors');
 
-const app = express();
-app.use(bodyParser.json());
-
-const corsOptions = {
-  origin: 'https://pag-doar.vercel.app/sms'
-};
-app.use(cors(corsOptions));
-
-const accountSid = 'ACca6ad5ddc85bc3e89dbf60d1cdac693a';
-const authToken = 'fa3085868cac4b3351fe4dd4a1d34abc';
-const client = new twilio(accountSid, authToken);
-
-app.get('/status', (req, res) => {
-  res.send('Server is running on Vercel');
-});
-
-app.post('/send-sms', async (req, res) => {
-  const { numero, mensagem } = req.body;
-
-  console.log('Request body:', req.body);
-
-  try {
-    const message = await client.messages.create({
-      body: mensagem,
-      from: '+13203773783',
-      to: numero,
-    });
-
-    console.log('Twilio response:', message);
-    res.status(200).send(message.sid);
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).send(error.message);
-  }
-});
-
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const bodyParser = require('body-parser'); 
+const twilio = require('twilio'); 
+const express = require('express'); 
+const cors = require('cors'); 
+  
+const app = express(); 
+app.use(bodyParser.json()); 
+  
+const corsOptions = { 
+  origin: 'https://pag-doar.vercel.app/sms' 
+}; 
+app.use(cors(corsOptions)); 
+  
+const accountSid = 'ACca6ad5ddc85bc3e89dbf60d1cdac693a'; 
+const authToken = 'fa3085868cac4b3351fe4dd4a1d34abc'; 
+const client = new twilio(accountSid, authToken); 
+  
+app.get('/status', (req, res) => { 
+  res.send('Servidor em execução'); 
+}); 
+  
+app.post('/send-sms', async (req, res) => { 
+  const { numero, mensagem } = req.body; 
+  
+  console.log('Request body:', req.body); 
+  
+  try { 
+    const message = await client.messages.create({ 
+      body: mensagem, 
+      from: '+13203773783', 
+      to: numero, 
+    }); 
+  
+    console.log('Twilio response:', message); 
+    res.status(200).send(message.sid); 
+  } catch (error) { 
+    console.error('Error:', error); 
+    res.status(500).send(error.message); 
+  } 
+}); 
+  
+const PORT = process.env.PORT || 3001; 
+app.listen(PORT, () => { 
+  console.log(`Servidor em execução na porta ${PORT}`); 
+}); 
