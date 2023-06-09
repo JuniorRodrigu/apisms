c395c467aabf4cef9eda91adc96f98df2614c916
JuniorRodrigu <juniorrodrigues1717@gmail.com>
2023-06-09 00:33:40 -0300

    mais um teste

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors());
 
-const accountSid = 'ACca6ad5ddc85bc3e89dbf60d1cdac693a';
-const authToken = 'fa3085868cac4b3351fe4dd4a1d34abc';
-const client = new twilio(accountSid, authToken);
-const cors = require('cors');
 const corsOptions = {
   origin: 'http://localhost:3000'
 };
 app.use(cors(corsOptions));
+
+const accountSid = 'ACca6ad5ddc85bc3e89dbf60d1cdac693a';
+const authToken = 'fa3085868cac4b3351fe4dd4a1d34abc';
+const client = new twilio(accountSid, authToken);
+
 app.get('/status', (req, res) => {
   res.send('Server is running on Vercel');
 });
