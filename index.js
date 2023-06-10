baf405d514ea319550e238a653629e49e8c8b1e8
JuniorRodrigu <juniorrodrigues1717@gmail.com>
2023-06-10 06:10:55 -0300

    2

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 
 // Adicionando os cabeçalhos CORS
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://pag-doar.vercel.app');
+  res.header('Access-Control-Allow-Origin', 'https://pag-doar.vercel.app/sms');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
