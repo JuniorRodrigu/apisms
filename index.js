915c92432d8ad0d7ad2e1902a526934d666c31e7
JuniorRodrigu <juniorrodrigues1717@gmail.com>
2023-06-10 00:19:40 -0300

    inicio

--- index.js
+++ index.js
@@ -1,45 +1,45 @@
-const bodyParser = require('body-parser'); 
-const twilio = require('twilio'); 
-const express = require('express'); 
-const cors = require('cors'); 
+const bodyParser = require('body-parser');
+const twilio = require('twilio');
+const express = require('express');
+const cors = require('cors');
 
-const app = express(); 
-app.use(bodyParser.json()); 
+const app = express();
+app.use(bodyParser.json());
 
-const corsOptions = { 
+const corsOptions = {
   origin: 'https://pag-doar.vercel.app/sms' 
-}; 
-app.use(cors(corsOptions)); 
+};
+app.use(cors(corsOptions));
 
-const accountSid = 'ACca6ad5ddc85bc3e89dbf60d1cdac693a'; 
-const authToken = 'fa3085868cac4b3351fe4dd4a1d34abc'; 
-const client = new twilio(accountSid, authToken); 
+const accountSid = 'ACca6ad5ddc85bc3e89dbf60d1cdac693a';
+const authToken = 'fa3085868cac4b3351fe4dd4a1d34abc';
+const client = new twilio(accountSid, authToken);
 
-app.get('/status', (req, res) => { 
+app.get('/status', (req, res) => {
   res.send('Servidor em execução'); 
-}); 
+});
 
-app.post('/send-sms', async (req, res) => { 
-  const { numero, mensagem } = req.body; 
+app.post('/send-sms', async (req, res) => {
+  const { numero, mensagem } = req.body;
 
-  console.log('Request body:', req.body); 
+  console.log('Request body:', req.body);
 
-  try { 
-    const message = await client.messages.create({ 
-      body: mensagem, 
-      from: '+13203773783', 
-      to: numero, 
-    }); 
+  try {
+    const message = await client.messages.create({
+      body: mensagem,
+      from: '+13203773783',
+      to: numero,
+    });
 
-    console.log('Twilio response:', message); 
-    res.status(200).send(message.sid); 
-  } catch (error) { 
-    console.error('Error:', error); 
-    res.status(500).send(error.message); 
-  } 
-}); 
+    console.log('Twilio response:', message);
+    res.status(200).send(message.sid);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send(error.message);
+  }
+});
 
-const PORT = process.env.PORT || 3001; 
-app.listen(PORT, () => { 
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
   console.log(`Servidor em execução na porta ${PORT}`); 
-}); 
+});
